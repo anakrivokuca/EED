3e63a462cee84adc8ff3c375a9c9041b285a588a
anakrivokuca <anakrivokuca@gmail.com>
2014-01-03 10:20:58 +0100

    Add validation rules to User class

--- src/EED.Domain/User.cs
+++ src/EED.Domain/User.cs
@@ -13,15 +13,28 @@ namespace EED.Domain
     {
         [HiddenInput(DisplayValue = false)]
         public virtual int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter name.")]
         public virtual string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter surname.")]
         public virtual string Surname { get; set; }
+
         public virtual string Email { get; set; }
+
         public virtual string State { get; set; }
+
+        [Required(ErrorMessage = "Please enter country.")]
         public virtual string Country { get; set; }
+
         [DisplayName("Phone Number")]
         public virtual string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Please enter username.")]
         public virtual string Username { get; set; }
+
         [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Please enter password.")]
         public virtual string Password { get; set; }
     }
 }
