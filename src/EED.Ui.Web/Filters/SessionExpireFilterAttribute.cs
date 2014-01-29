73631e164605cb7b74bcc856e138e5f750708612
anakrivokuca <anakrivokuca@gmail.com>
2014-01-29 23:58:18 +0100

    Set session timeout value and verify if session has expired
    - Add SessionExpireFilterAttribute class and add created attribute to DistrictType controller to redirect action to Project controller if session expires
    - Add session timeout attribute to Web.config
    - Add project name to Layout
    - Minor style changes

---
+++ src/EED.Ui.Web/Filters/SessionExpireFilterAttribute.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EED.Ui.Web.Filters
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, 
+        Inherited = true, AllowMultiple = true)]
+    public class SessionExpireFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Session["projectId"] == null)
+            {
+                // check if a new session id was generated
+                filterContext.Result = new RedirectResult("/Project/List");
+                return;
+            }
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
